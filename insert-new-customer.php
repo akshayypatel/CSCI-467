5eb96c9a782eaed9481ee7320508aa9e088be855
akshayypatel <z1785771@students.niu.edu>
2019-04-07 13:02:16 -0500

    Got the Create New Customer page working on the backend

--- insert-new-customer.php
+++ insert-new-customer.php
@@ -4,29 +4,55 @@
 	$password = "1995Jan10";
 	$pdo = new PDO($dsn, $username, $password);
 	
-	// Save current time
-	$date = date("h:i:sa");
+	// Save current time as temp password
+	$customer_password = date("h:i:sa");
+
     // Insert new account
-	$results = $pdo->prepare("INSERT INTO customer_account (companyName, quoteType, managerEmailAddress, managerPhoneNumber, password) VALUES (?, ?, ?, ?, ?);");
+	$result = $pdo->prepare("INSERT INTO customer_account (companyName, quoteType, managerEmailAddress, managerPhoneNumber, password) VALUES (?, ?, ?, ?, ?);");
+	
+	$managerEmailAddress = "apate29@niu.edu";
+	$managerPhoneNumber = "8159314345";
+	
 	if (isset( $_POST['company-name'], $_POST['quote-type']))
 	{	
-		$results->execute(array($_POST['company-name'], "auto", "gonzalo@gmail.com", "1234567890", $date ));
-    } else {
+		$result->execute(array($_POST['company-name'], $_POST['quote-type'], $managerEmailAddress, $managerPhoneNumber, $customer_password));
+    } 
+    else {
 		header("Location: error.html");
 	}
 
-	// select * from customer_account where password = "10:16:25pm";
-	$results = $pdo->prepare("SELECT * FROM customer_account WHERE password = ?");
-	$results->execute(array($date));
+	//insert billing address
+	$result = $pdo->prepare("INSERT INTO address (customerID, addressType, street, city, state, zipCode) VALUES ((SELECT MAX(customerID) FROM customer_account), ?, ?, ?, ?, ?);");
 
+	if (isset( $_POST['billing-street'], $_POST['billing-city'], $_POST['billing-state'], $_POST['billing-zip']))
+	{	
+		$result->execute(array("Billing", $_POST['billing-street'], $_POST['billing-city'], $_POST['billing-state'], $_POST['billing-zip'] ));
 
-    // insert new address
-    // $results = $pdo->prepare("INSERT INTO customer_account (companyName, quoteType, managerEmailAddress, managerPhoneNumber, password) VALUES (?, ?, ?, ?, ?);");
-	// if (isset($_POST['company-name'], $_POST['quote-type'], $_POST['quantity'], $_POST['listing-price'], $_POST['manufacturer'] ))
-	// {	
-    //     $date = date("h:i:sa");
-	// 	$results->execute(array($_POST['company-name'], $_POST['quote-type'], "gonzalo@gmail.com", "1234567890", $date ));
-    // }
-    // Redirect to success page
-	header("Location: part-created.html");
+    } 
+    else {
+		header("Location: error.html");
+	}
+
+	//insert shipping address
+	$result = $pdo->prepare("INSERT INTO address (customerID, addressType, street, city, state, zipCode) VALUES ((SELECT MAX(customerID) FROM customer_account), ?, ?, ?, ?, ?);");
+
+	if (isset( $_POST['shipping-street'], $_POST['shipping-city'], $_POST['shipping-state'], $_POST['shipping-zip']))
+	{	
+		$result->execute(array("Shipping", $_POST['shipping-street'], $_POST['shipping-city'], $_POST['shipping-state'], $_POST['shipping-zip'] ));
+    } 
+    else {
+		header("Location: error.html");
+	}
+
+	//insert representative
+	$result = $pdo->prepare("INSERT INTO customer_representative (phoneNumber, firstName, lastName, email, customerID) VALUES (?, ?, ?, ?,(SELECT MAX(customerID) FROM customer_account));");
+
+	if (isset( $_POST['phone'], $_POST['representative-first-name'], $_POST['representative-last-name'], $_POST['email'] ))
+	{	
+		$result->execute(array( $_POST['phone'], $_POST['representative-first-name'], $_POST['representative-last-name'], $_POST['email'] ));
+    } 
+    else {
+		header("Location: error.html");
+	}
+
 ?>
--- insert-new-part.php
+++ insert-new-part.php
@@ -4,11 +4,11 @@
 	$password = "1995Jan10";
 	$pdo = new PDO($dsn, $username, $password);
 				
-	$results = $pdo->prepare("INSERT INTO inventory_part (partName, partDescription, quantity, listingPrice, manufacturer_name, comments) VALUES (?, ?, ?, ?, ?, ?);");
+	$results = $pdo->prepare("INSERT INTO inventory_part (partName, partDescription, quantity, listingPrice, manufacturer_name) VALUES (?, ?, ?, ?, ?);");
 	if (isset($_POST['part-name'], $_POST['part-description'], $_POST['quantity'], $_POST['listing-price'], $_POST['manufacturer'] ))
 	{	
 		$results->execute(array($_POST['part-name'], $_POST['part-description'], $_POST['quantity'], $_POST['listing-price'], $_POST['manufacturer'] ));
     }
     // Redirect to success page
-	header("Location: part-created.html");
+	header("Location: successful.html");
 ?>

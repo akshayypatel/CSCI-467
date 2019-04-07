c855a98fadcb09a7026579484f123a2f3075a41b
Gonzalo Pantoja <gnpantoja@aol.com>
2019-04-06 22:40:57 -0500

    Ready for merge

--- insert-new-customer.php
+++ insert-new-customer.php
@@ -3,14 +3,23 @@
 	$username = "z1785771";
 	$password = "1995Jan10";
 	$pdo = new PDO($dsn, $username, $password);
-    
+	
+	// Save current time
+	$date = date("h:i:sa");
     // Insert new account
 	$results = $pdo->prepare("INSERT INTO customer_account (companyName, quoteType, managerEmailAddress, managerPhoneNumber, password) VALUES (?, ?, ?, ?, ?);");
-	if (isset($_POST['company-name'], $_POST['quote-type'] ))
+	if (isset( $_POST['company-name'], $_POST['quote-type']))
 	{	
-        $date = date("h:i:sa");
-		$results->execute(array("discoverrrrr", "auto", "gonzalo@gmail.com", "1234567890", $date ));
-    }
+		$results->execute(array($_POST['company-name'], "auto", "gonzalo@gmail.com", "1234567890", $date ));
+    } else {
+		header("Location: error.html");
+	}
+
+	// select * from customer_account where password = "10:16:25pm";
+	$results = $pdo->prepare("SELECT * FROM customer_account WHERE password = ?");
+	$results->execute(array($date));
+
+
     // insert new address
     // $results = $pdo->prepare("INSERT INTO customer_account (companyName, quoteType, managerEmailAddress, managerPhoneNumber, password) VALUES (?, ?, ?, ?, ?);");
 	// if (isset($_POST['company-name'], $_POST['quote-type'], $_POST['quantity'], $_POST['listing-price'], $_POST['manufacturer'] ))

7e7e2bcf4e5ec722f3bad258c44686931fe00b41
Gonzalo Pantoja <gnpantoja@aol.com>
2019-04-06 21:48:16 -0500

    Almost done adding php to new customer

--- insert-new-customer.php
+++ insert-new-customer.php
@@ -6,10 +6,10 @@
     
     // Insert new account
 	$results = $pdo->prepare("INSERT INTO customer_account (companyName, quoteType, managerEmailAddress, managerPhoneNumber, password) VALUES (?, ?, ?, ?, ?);");
-	if (isset($_POST['company-name'], $_POST['quote-type'], $_POST['quantity'], $_POST['listing-price'], $_POST['manufacturer'] ))
+	if (isset($_POST['company-name'], $_POST['quote-type'] ))
 	{	
         $date = date("h:i:sa");
-		$results->execute(array($_POST['company-name'], $_POST['quote-type'], "gonzalo@gmail.com", "1234567890", $date ));
+		$results->execute(array("discoverrrrr", "auto", "gonzalo@gmail.com", "1234567890", $date ));
     }
     // insert new address
     // $results = $pdo->prepare("INSERT INTO customer_account (companyName, quoteType, managerEmailAddress, managerPhoneNumber, password) VALUES (?, ?, ?, ?, ?);");

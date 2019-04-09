020fbfb555c908dce2e95354824dd30f10890c66
akshayypatel <z1785771@students.niu.edu>
2019-04-09 15:49:52 -0500

    Added scripts

--- database/queries.sql
+++ database/queries.sql
@@ -1,3 +1,21 @@
+-- Insert Customer Accounts
+
+INSERT INTO customer_account VALUES (1, "Facebook", "Auto", "apatel29@niu.edu", "815-754-8856", "MarkZuckerberg", "blueBook$blind-19", "");
+INSERT INTO customer_account VALUES (2, "Microsoft", "Manual", "gpantoja@niu.edu", "815-312-5674", "BillieG", "uS0ft-$harvard!", "");
+INSERT INTO customer_account VALUES (3, "Discover", "Auto", "gpantoja@niu.edu", "815-312-5674", "RogerHockchild", "#code_orange", "They have escalated the CS program at NIU");
+INSERT INTO customer_account VALUES (4, "Apple", "Manual", "apatel29@niu.edu", "815-754-8856", "TimmieApp", "LookWithA#C2011", "Not Steve Jobs, but he's okay..");
+
+-- Insert Address
+
+INSERT INTO address VALUES (1, 1, "Billing", "1 Hacker Way", "Menlo Park", "CA", "94025");
+INSERT INTO address VALUES (2, 1, "Shipping", "191 N. Wacker Drive", "Chicago", "IL", "60606");
+INSERT INTO address VALUES (3, 2, "Billing", "One Microsoft Way", "Redmond", "WA", "98052");
+INSERT INTO address VALUES (4, 2, "Shipping", "3025 Highland Parkway", "Downers Grove", "IL", "60515");
+INSERT INTO address VALUES (5, 3, "Billing", "2500 Lake Cook Rd", "Riverwoods", "IL", "60015");
+INSERT INTO address VALUES (6, 3, "Shipping", "217 Normal Rd", "Dekalv", "IL", "60115");
+INSERT INTO address VALUES (7, 4, "Billing", "1 Infinite Loop", "Cupertino", "CA", "95014");
+INSERT INTO address VALUES (8, 4, "Shipping", "1 Apple Park Way", "Cupertino", "CA", "95014");
+
 -- Query to view company reps
 SELECT companyName, CONCAT(firstName, ' ', lastName) AS representative, email, username, password
 FROM `customer_account`
